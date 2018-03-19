d30c83674a04742f13943f4d85949e384aabaed3
Dillon <dragdi15@gmail.com>
2018-03-19 14:18:35 -0400

    Update ArrayBag.h

--- ArrayBag.h
+++ ArrayBag.h
@@ -1,6 +1,3 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
-
 ///////////////////////////
 //FILE NAME: ArrayBag.h//
 ///////////////////////////
